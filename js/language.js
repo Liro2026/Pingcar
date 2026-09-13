57a6b34c124df9b1c8ff9a30cd44b91d8afa5e3c
Liro2026 <lirim.salihu2017@gmail.com>
2026-09-13 19:31:08 +0200

    Add Greek language support to language.js
    Add Greek language support to language.js

--- js/language.js
+++ js/language.js
@@ -4,7 +4,7 @@
 
   const supportedLanguages = [
     "de", "en", "fr", "it", "sq", "sr", "es", 
-    "tr", "sk", "hr", "hu", "pt", "mk", "nl","sv","ro"
+    "tr", "sk", "hr", "hu", "pt", "mk", "nl","sv","ro", "el"
   ];
 
   const defaultLanguage = "de";
@@ -24,7 +24,7 @@
     IT: "it",
     AL: "sq", XK: "sq",
     RS: "sr", ES: "es", TR: "tr", SK: "sk",
-    HR: "hr", HU: "hu", PT: "pt", MK: "mk", NL: "nl", SV: "sv", RO: "ro"
+    HR: "hr", HU: "hu", PT: "pt", MK: "mk", NL: "nl", SV: "sv", RO: "ro", EL: "el"
   };
 
   async function detectLanguageFromIP() {
