dd41459c184f573c9c3568d02f4cd4192d8429b7
Liro2026 <lirim.salihu2017@gmail.com>
2026-09-14 23:05:28 +0200

    Add Czech language support to supportedLanguages
    Add Czech language support to supportedLanguages

--- js/language.js
+++ js/language.js
@@ -4,7 +4,7 @@
 
   const supportedLanguages = [
     "de", "en", "fr", "it", "sq", "sr", "es", 
-    "tr", "sk", "hr", "hu", "pt", "mk", "nl","sv","ro", "el", "pl"
+    "tr", "sk", "hr", "hu", "pt", "mk", "nl","sv","ro", "el", "pl","cs"
   ];
 
   const defaultLanguage = "de";
@@ -24,7 +24,7 @@
     IT: "it",
     AL: "sq", XK: "sq",
     RS: "sr", ES: "es", TR: "tr", SK: "sk",
-    HR: "hr", HU: "hu", PT: "pt", MK: "mk", NL: "nl", SV: "sv", RO: "ro", EL: "el", PL: "pl"
+    HR: "hr", HU: "hu", PT: "pt", MK: "mk", NL: "nl", SV: "sv", RO: "ro", EL: "el", PL: "pl", CS: "cs"
   };
 
   async function detectLanguageFromIP() {
