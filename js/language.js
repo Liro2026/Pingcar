3c54787bddc692abc1ffb1417f8fcb446b2a8b7e
Liro2026 <lirim.salihu2017@gmail.com>
2026-09-14 19:21:35 +0200

    Add Polish language support to language.js
    Add Polish language support to language.js

--- js/language.js
+++ js/language.js
@@ -4,7 +4,7 @@
 
   const supportedLanguages = [
     "de", "en", "fr", "it", "sq", "sr", "es", 
-    "tr", "sk", "hr", "hu", "pt", "mk", "nl","sv","ro", "el"
+    "tr", "sk", "hr", "hu", "pt", "mk", "nl","sv","ro", "el", "pl"
   ];
 
   const defaultLanguage = "de";
@@ -24,7 +24,7 @@
     IT: "it",
     AL: "sq", XK: "sq",
     RS: "sr", ES: "es", TR: "tr", SK: "sk",
-    HR: "hr", HU: "hu", PT: "pt", MK: "mk", NL: "nl", SV: "sv", RO: "ro", EL: "el"
+    HR: "hr", HU: "hu", PT: "pt", MK: "mk", NL: "nl", SV: "sv", RO: "ro", EL: "el", PL: "pl"
   };
 
   async function detectLanguageFromIP() {
