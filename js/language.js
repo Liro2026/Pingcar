ba6119222d5ec76cc39e8580f91765dec5b74cd5
Liro2026 <lirim.salihu2017@gmail.com>
2026-09-16 21:34:43 +0200

    Add support for Chinese language in language.js
    Add support for Chinese language in language.js

--- js/language.js
+++ js/language.js
@@ -4,7 +4,7 @@
 
   const supportedLanguages = [
     "de", "en", "fr", "it", "sq", "sr", "es", 
-    "tr", "sk", "hr", "hu", "pt", "mk", "nl","sv","ro", "el", "pl","cs","sw","ar"
+    "tr", "sk", "hr", "hu", "pt", "mk", "nl","sv","ro", "el", "pl","cs","sw","ar","zh"
   ];
 
   const defaultLanguage = "de";
@@ -24,7 +24,8 @@
     IT: "it",
     AL: "sq", XK: "sq",
     RS: "sr", ES: "es", TR: "tr", SK: "sk",
-    HR: "hr", HU: "hu", PT: "pt", MK: "mk", NL: "nl", SV: "sv", RO: "ro", EL: "el", PL: "pl", CS: "cs",SW: "sw",AR: "ar"
+    HR: "hr", HU: "hu", PT: "pt", MK: "mk", NL: "nl", SV: "sv", RO: "ro", EL: "el", PL: "pl", CS: "cs",SW: "sw",AR: "ar",
+    ZH: "zh"
   };
 
   async function detectLanguageFromIP() {
