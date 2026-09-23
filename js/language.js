c4fb0e135a28fa93128b375f1d28a5bdd81e3cf7
Liro2026 <lirim.salihu2017@gmail.com>
2026-09-23 19:58:46 +0200

    Add Danish language support to the list
    Add Danish language support to the list

--- js/language.js
+++ js/language.js
@@ -5,7 +5,7 @@
   const supportedLanguages = [
     "de", "en", "fr", "it", "sq", "sr", "es", 
     "tr", "sk", "hr", "hu", "pt", "mk", "nl","sv","ro", "el", "pl","cs","sw","ar","zh","ja",
-    "bg", "ru", "hi"
+    "bg", "ru", "hi", "da"
   ];
 
   const defaultLanguage = "de";
@@ -26,7 +26,7 @@
     AL: "sq", XK: "sq",
     RS: "sr", ES: "es", TR: "tr", SK: "sk",
     HR: "hr", HU: "hu", PT: "pt", MK: "mk", NL: "nl", SV: "sv", RO: "ro", EL: "el", PL: "pl", CS: "cs",SW: "sw",AR: "ar",
-    ZH: "zh", JA: "ja", BG: "bg", RU: "ru", HI: "hi"
+    ZH: "zh", JA: "ja", BG: "bg", RU: "ru", HI: "hi", DA: "da"
   };
 
   async function detectLanguageFromIP() {
